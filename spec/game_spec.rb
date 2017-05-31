77ad05e49876e3f2ade65e1606c62f58961fe51f
Melissa Kaulfuss <melissacor3lfuss@gmail.com>
2017-05-31 13:49:35 +1000

    Initial commit

---
+++ spec/game_spec.rb
@@ -0,0 +1,18 @@
+require 'game'
+
+describe Game do
+
+  it 'receives a word string' do
+
+  end
+
+  p 'sanitizes the word string' do
+  end
+
+  p 'throws an exception when receiveing an empty string' do
+  end
+
+  p 'throws an exception when receiveing nil' do
+  end
+
+end
