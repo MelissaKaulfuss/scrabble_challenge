d567994026fa4584f97f9630c24c1531ab9e7079
Melissa Kaulfuss <melissacor3lfuss@gmail.com>
2017-06-22 10:31:41 +1000

    Handle nil values being passed to score by coercing word to String

--- lib/empty_word.rb
+++
@@ -1,5 +0,0 @@
-class NilClass
-
-
-
-end
--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,23 +1,23 @@
 class Scrabble
 
-attr_reader :word
+  attr_reader :word
 
-DICTIONARY = {
-  "A" => 1, "B" => 3, "C" => 3, "D" => 2,
-  "E" => 1, "F" => 4, "G" => 2, "H" => 4,
-  "I" => 1, "J" => 8, "K" => 5, "L" => 1,
-  "M" => 3, "N" => 1, "O" => 1, "P" => 3,
-  "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
-  "U" => 1, "V" => 4, "W" => 4, "X" => 8,
-  "Y" => 4, "Z" => 10
-}
+  DICTIONARY = {
+    "A" => 1, "B" => 3, "C" => 3, "D" => 2,
+    "E" => 1, "F" => 4, "G" => 2, "H" => 4,
+    "I" => 1, "J" => 8, "K" => 5, "L" => 1,
+    "M" => 3, "N" => 1, "O" => 1, "P" => 3,
+    "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
+    "U" => 1, "V" => 4, "W" => 4, "X" => 8,
+    "Y" => 4, "Z" => 10
+  }
 
   def score(word)
     @word = String(word)
-      formatted_word = @word.upcase.strip
-      letters_to_score = formatted_word.chars
+    formatted_word = @word.upcase.strip
+    letters_to_score = formatted_word.chars
 
-      letter_scores_to_add = letters_to_score.map { | char | DICTIONARY[char] }
-      final_score = letter_scores_to_add.inject(0) { | sum, a| sum + a }
+    letter_scores_to_add = letters_to_score.map { | char | DICTIONARY[char] }
+    final_score = letter_scores_to_add.inject(0) { | sum, a| sum + a }
   end
 end
